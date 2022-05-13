93c3134a4e6d58fdd1417ec184ebde54bbff0540
Espen Boman Fosseide <espenbfo@stud.ntnu.no>
2022-05-13 19:28:01 +0200

    feat: more styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import {Grid} from "./components/grid";
+import "./App.css"
 import {BingoSquare} from "./components/bingo-square";
 
 function App() {
   return (
-    <div className="screen">
-      <header className="bg-gray-800">
-          Hello
+    <div className="app">
+      <header className="header">
+          Bingo
       </header>
 
         <Grid>
-              <BingoSquare active={true} connectedVertical={false} connectedHorizontal={false}/>
+              <BingoSquare active={true} connectedVertical={false} connectedHorizontal={false} text={"hello"}/>
               <BingoSquare active={true} connectedVertical={false} connectedHorizontal={false}/>
               <BingoSquare active={true} connectedVertical={false} connectedHorizontal={false}/>
               <BingoSquare active={true} connectedVertical={false} connectedHorizontal={false}/>
--- src/components/bingo-square.tsx
+++ src/components/bingo-square.tsx
@@ -1,7 +1,12 @@
 import clsx from "clsx";
 import "./bingo-square.css"
-type Props = {active: boolean, connectedVertical: boolean, connectedHorizontal: boolean}
+import {useState} from "react";
 
-export const BingoSquare = ({active, connectedHorizontal, connectedVertical}: Props) => {
-    return (<div className={clsx("square", active && "active")} />);
+type Props = { active: boolean, connectedVertical: boolean, connectedHorizontal: boolean, text?: string }
+
+export const BingoSquare = ({active, connectedHorizontal, connectedVertical, text}: Props) => {
+    const [active2, setActive] = useState(false)
+    return (<div className={clsx("square")} onClick={() => setActive(!active2)}>
+        <div className={clsx("square-inside", active2 && "active")}><span>{text}</span></div>
+    </div>);
 }
